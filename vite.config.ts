ff19d980628ed2148e24be6288aa0f4a35ae4462
Chloé halloin <119426862+ChloeHal@users.noreply.github.com>
2025-07-18 10:10:22 +0200

    Bug React

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react"; // Plugin standard maintenant
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/PortfolioChloHal/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  base: "/",
+});
